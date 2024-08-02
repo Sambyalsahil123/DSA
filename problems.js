9f46bc357d0606414d48d8b116d9a3700fd15250
SAHIL SAMBYAL <104450436+Sambyalsahil123@users.noreply.github.com>
2024-08-02 12:58:37 +0530

    Update problems.js

--- problems.js
+++ problems.js
@@ -1,1 +1,29 @@
 1+"2" - "1" // 11
+
+// - Difference b/w Null or Undefined 
+
+// null: Represents the intentional absence of any object value. It's typically used to indicate that a variable should have no value.
+// i.e =>  let a = null 
+
+// undefined: Indicates that a variable has been declared but not yet assigned a value. It can also be the value of a function parameter that wasn't passed.
+// i.e =>  let a , let a = undefined 
+
+
+ // Code examples -
+   
+function difference(name, age = null) {
+  return `Hi my name is ${name} and my age is ${age}`;
+}
+console.log(difference("Sahil", 12));
+
+
+function difference(name, age = undefined) {
+  return `Hi my name is ${name} and my age is ${age}`;
+}
+console.log(difference("Sahil", 12));
+
+// Note: There is also Type difference :
+
+console.log(typeof null); // Output: object
+console.log(typeof undefined); // Output: undefined
+
